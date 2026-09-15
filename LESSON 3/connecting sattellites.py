697e155550eee42e761619a778fce20df9c9134d
hibaba-cloud <aamirwarsi2022@gmail.com>
2026-09-15 19:25:07 +0100

    Add files via upload

--- LESSON 3/connecting sattellites.py
+++ LESSON 3/connecting sattellites.py
@@ -2,15 +2,34 @@ import pgzrun
 import random
 WIDTH=400
 HEIGHT=400
+lines=[]
+sattellites=[]
+def create_sattelites():
+   for i in range (25):
+         
+         sattellite=Actor("satallite")
+         sattellite.x=random.randint(0,400)
+         sattellite.y=random.randint(0,400)
+         sattellites.append(sattellite)
+  
+create_sattelites()   
 def draw():
     screen.fill("white")
     screen.blit("background",(0,0))
-    for i in range (25):
-      
-      sattellite=Actor("satallite")
-      sattellite.x=random.randint(0,400)
-      sattellite.y=random.randint(0,400)
-      sattellite.draw()
+    for i in sattellites:
+        i.draw()
+    if len(lines)>2:
+      for i in range(0,len(lines)-1,2):
+        screen.draw.line(lines[i],lines[i+1],"red")  
+    
+
+def on_mouse_down (pos):
+   for i in sattellites:
+      if i.collidepoint(pos):
+         lines.append(i.pos)
+   print(lines)
+   
+   
     
 
 
--- LESSON 3/dot.py
+++ LESSON 3/dot.py
@@ -2,14 +2,38 @@ import pgzrun
 import random
 WIDTH=400
 HEIGHT=400
+lines=[]
+dotts=[]
+def create_dots():
+   for i in range (25):
+      dott=Actor("dot 2")
+      dott.x=random.randint(0,400)
+      dott.y=random.randint(0,400)
+      dotts.append(dott)
+         
+         
+        
+create_dots()
+
+            
+    
 def draw():
     screen.fill("white")
-    #screen.blit("background",(0,0))
-    for i in range (50):
-       dott=Actor("dot")
-       dott.x=random.randint(0,400)
-       dott.y=random.randint(0,400)
-       dott.draw()
+    screen.blit("background",(0,0))
+    for i in dotts:
+        i.draw()
+    if len(lines)>2:
+      for i in range(0,len(lines)-1,2):
+        screen.draw.line(lines[i],lines[i+1],"blue") 
+
+
+
+def on_mouse_down (pos):
+   for i in dotts:
+      if i.collidepoint(pos):
+         lines.append(i.pos)
+   print(lines)
+    
 
 
 
